0ed17164ab880b31db0503e060059647a14f8f0e
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2020-08-22 01:18:59 +0200

    Improve apt detection

--- .dotfiles/bootstrap.sh
+++ .dotfiles/bootstrap.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-os=$(awk -F= '/^NAME/{print $2}' /etc/os-release | sed 's/"//g')
+os=$(awk -F= '/^NAME/{print $2}' /etc/os-release | sed -e 's/"\|GNU\|\/\|Linux//g' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+hasApt=$(which apt)
 
 echo "> updating system\n"
-if [ "$os" = "Ubuntu" ]; then
+if [ "$hasApt" ]; then
 	sudo apt update
 	sudo apt upgrade -y
 	sudo apt dist-upgrade -y
 fi
 
 echo "> installing dependencies\n"
-if [ "$os" = "Ubuntu" ]; then
+if [ "$hasApt" ]; then
 	sudo apt install curl git -y
 fi
 
 echo "> installing zsh\n"
-if [ "$os" = "Ubuntu" ]; then
+if [ "$hasApt" ]; then
 	sudo apt install zsh -y
 fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended --keep-zshrc"
@@ -23,7 +24,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo "> installing vim\n"
-if [ "$os" = "Ubuntu" ]; then
+if [ "$hasApt" ]; then
 	sudo apt install neovim -y
 fi
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
