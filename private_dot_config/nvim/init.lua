3caade9b67451ec7ba1ebba188bc1486faeb3afa
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2023-10-13 16:22:51 +0200

    feat: Init LazyNvim using the starter template

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,25 +1,2 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.mapleader = " "
-
--- Load lazy.nvim
-require("lazy").setup({
-  "github/copilot.vim"
-})
-
--- Enable Copilot for all filetypes by setting the * key to true
-vim.g.copilot_filetypes = { ["*"] = true }
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
